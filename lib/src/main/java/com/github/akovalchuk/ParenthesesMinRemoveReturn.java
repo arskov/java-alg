9d4713042ffdb423af13ba8745e0ba6d3790feeb
Arseny Kovalchuk <ars.kov@gmail.com>
2021-01-30 01:22:49 +0300

    Leetcode. 301. Remove Invalid Parentheses

---
+++ lib/src/main/java/com/github/akovalchuk/ParenthesesMinRemoveReturn.java
@@ -0,0 +1,70 @@
+package com.github.akovalchuk;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 301. Remove Invalid Parentheses
+ */
+public class ParenthesesMinRemoveReturn {
+    
+    private boolean isValid(String s) {
+        if (s.isEmpty()) 
+            return true;
+        int balance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                balance++;
+            } else if (s.charAt(i) == ')') {
+                if (balance == 0)
+                    return false;
+                balance--;
+            }
+        }
+        return balance == 0;
+    }
+    
+    public List<String> removeInvalidParentheses(String s) {
+        if (s == null || s.isEmpty()) {
+            return List.of("");
+        }
+        List<String> result = new ArrayList<>();
+        Queue<String> queue = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+        boolean found = false;
+        queue.offer(s);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                String tmp = queue.poll();
+                if (isValid(tmp)) {
+                    found = true;
+                    result.add(tmp);
+                } else {
+                    for (int j = 0; j < tmp.length(); j++) {
+                        if (tmp.charAt(j) == '(' || tmp.charAt(j) == ')') {
+                            String newStr = tmp.substring(0, j) + tmp.substring(j + 1);
+                            if (!visited.contains(newStr)) {
+                                visited.add(newStr);
+                                queue.offer(newStr);
+                            }
+                        }
+                    }
+                }
+            }
+            if (found) break;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        var solution = new ParenthesesMinRemoveReturn();
+        System.out.println(solution.removeInvalidParentheses("()())()"));
+        System.out.println(solution.removeInvalidParentheses("(a)())()"));
+    }
+}
