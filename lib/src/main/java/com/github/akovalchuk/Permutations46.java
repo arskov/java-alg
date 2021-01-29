99a1e442b08737cb990ceea9d5132d80e5e7d317
Arseny Kovalchuk <ars.kov@gmail.com>
2021-01-29 15:54:15 +0300

    Leetcode. 46. Permutations

---
+++ lib/src/main/java/com/github/akovalchuk/Permutations46.java
@@ -0,0 +1,44 @@
+package com.github.akovalchuk;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Permutations46 {
+
+    private void generate(List<List<Integer>> out, int[] nums, int pos, int size) {
+        if (pos == size) {
+            var tmp = new ArrayList<Integer>(size);
+            for (int i = 0; i < nums.length; i++) {
+                tmp.add(nums[i]);
+            }
+            out.add(tmp);
+            return;
+        }
+        for (int i = pos; i < nums.length; i++) {
+            swap(nums, pos, i);
+            generate(out, nums, pos + 1, size);
+            swap(nums, pos, i);
+        }
+    }
+    
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+    
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null || nums.length == 0) return Collections.emptyList();
+        if (nums.length == 1) return List.of(List.of(nums[0]));
+        List<List<Integer>> out = new ArrayList<>();
+        generate(out, nums, 0, nums.length);
+        return out;
+    }
+
+    public static void main(String[] args) {
+        var solution = new Permutations46();
+        System.out.println(solution.permute(new int[]{1,2,3}));
+    }
+    
+}
