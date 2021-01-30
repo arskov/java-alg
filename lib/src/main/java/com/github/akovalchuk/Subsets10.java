b5237d242f8c55267c47a7cd6ad3ff97936a0b51
Arseny Kovalchuk <ars.kov@gmail.com>
2021-01-30 18:02:49 +0300

    Leetcode. 78. Subsets

---
+++ lib/src/main/java/com/github/akovalchuk/Subsets10.java
@@ -0,0 +1,32 @@
+package com.github.akovalchuk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 78. Subsets
+ */
+public class Subsets10 {
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null || nums.length == 0) return List.of(List.of());
+        var res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < (1 << nums.length); i++) {
+            var tmp = new ArrayList<Integer>();
+            for (int j = 0; j < nums.length; j++) {
+                int idx = 1 << j;
+                if ((i & idx) == idx) {
+                    tmp.add(nums[j]);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        var solution = new Subsets10();
+        System.out.println(solution.subsets(new int[] {1,2,3}));
+    }
+
+}
