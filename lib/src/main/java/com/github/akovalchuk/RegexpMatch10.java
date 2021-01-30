5262bf0fc4d55cbe1e7e2f70ae187a4aea091436
Arseny Kovalchuk <ars.kov@gmail.com>
2021-01-30 17:40:23 +0300

    Update for 10. Regular Expression Matching

--- lib/src/main/java/com/github/akovalchuk/RegexpMatch10.java
+++ lib/src/main/java/com/github/akovalchuk/RegexpMatch10.java
@@ -5,6 +5,8 @@ package com.github.akovalchuk;
  */
 public class RegexpMatch10 {
 
+    Boolean[][] memo;
+
     public boolean isMatchRecursion(String s, String p) {
         if (p == null || s == null)
             return false;
@@ -18,14 +20,54 @@ public class RegexpMatch10 {
         }
     }
 
+    private Boolean dp(int i, int j, String s, String p) {
+        if (memo[i][j] != null)
+            return memo[i][j];
+        Boolean result;
+        if (j == p.length()) {
+            result = (i == s.length());
+        } else {
+            Boolean first = (i < s.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.'));
+            if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                result = dp(i, j + 2, s, p) || (first && dp(i + 1, j, s, p));
+            } else {
+                result = first && dp(i + 1, j + 1, s, p);
+            }
+        }
+        memo[i][j] = result;
+        return result;
+    }
+    
+    public boolean isMatchRecursionMemo(String s, String p) {
+        if (p == null || s == null)
+            return false;
+        if (p.isEmpty())
+            return s.isEmpty();
+        memo = new Boolean[s.length() + 1][p.length() + 1];
+        return dp(0, 0, s, p);
+    }
+
     public static void main(String[] args) {
         var solution = new RegexpMatch10();
         System.out.println(solution.isMatchRecursion("sss", "ppp"));
+        System.out.println(solution.isMatchRecursionMemo("sss", "ppp"));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", "s"));
+        System.out.println(solution.isMatchRecursionMemo("sss", "s"));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", "s*"));
+        System.out.println(solution.isMatchRecursionMemo("sss", "s*"));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", ".*"));
+        System.out.println(solution.isMatchRecursionMemo("sss", ".*"));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", "."));
+        System.out.println(solution.isMatchRecursionMemo("sss", "."));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", ".."));
+        System.out.println(solution.isMatchRecursionMemo("sss", ".."));
+        System.out.println();
         System.out.println(solution.isMatchRecursion("sss", "..."));
+        System.out.println(solution.isMatchRecursionMemo("sss", "..."));
     }
 }
