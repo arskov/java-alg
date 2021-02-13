22edc2475a07cf74966ad289dbb9d1b51e28d9f1
Arseny Kovalchuk <ars.kov@gmail.com>
2021-02-13 17:55:05 +0300

    Leetcode. 238. Product of Array Except Self

---
+++ lib/src/main/java/com/github/akovalchuk/ProductOfArrayExceptSelf.java
@@ -0,0 +1,38 @@
+package com.github.akovalchuk;
+
+import java.util.Arrays;
+
+/**
+ * 238. Product of Array Except Self
+ */
+public class ProductOfArrayExceptSelf {
+    /*
+    3,4,5,4,2
+    
+    3   12   60  240 480
+    480 160  40  8   2
+    
+    ======
+    160 120  96  120 240
+    */
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) return nums;
+        int[] out = new int[nums.length];
+        int[] prodRev = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            prodRev[i] = (i == nums.length - 1 ? nums[i] : prodRev[i + 1] * nums[i]); 
+        }
+        int prodFv = 1;
+        for (int i = 0; i < nums.length; i++) {
+            out[i] = prodFv * (i == nums.length - 1 ? 1 : prodRev[i + 1]);
+            prodFv *= nums[i];
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+        var sol = new ProductOfArrayExceptSelf();
+        System.out.println(Arrays.toString(sol.productExceptSelf(new int[] {1,2,3,4})));
+        System.out.println(Arrays.toString(sol.productExceptSelf(new int[] {3,4,5,4,2})));
+    }
+}
