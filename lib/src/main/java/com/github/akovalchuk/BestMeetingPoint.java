ef98d766b2132ea32c1166ce39e17054a9ec8e95
Arseny Kovalchuk <ars.kov@gmail.com>
2021-03-01 23:56:30 +0300

    Leetcode. 296. Best Meeting Point

---
+++ lib/src/main/java/com/github/akovalchuk/BestMeetingPoint.java
@@ -0,0 +1,42 @@
+package com.github.akovalchuk;
+
+import java.util.ArrayList;
+
+/**
+ * 296. Best Meeting Point
+ */
+public class BestMeetingPoint {
+    
+    public int minTotalDistanceMNMN(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        var points = new ArrayList<int[]>();
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) points.add(new int[] {i, j});
+            }
+        }
+        int minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int dist = 0;
+                for (int[] point : points) {
+                    dist += Math.abs(point[0] - i) + Math.abs(point[1] - j);
+                }
+                minDist = Math.min(minDist, dist);
+            }
+        }
+        return minDist;
+    }
+
+    public static void main(String[] args) {
+        var sol = new BestMeetingPoint();
+        int[][] test = {
+            {1,0,0,0,1},
+            {0,0,0,0,0},
+            {0,0,1,0,0}
+        };
+        System.out.println(sol.minTotalDistanceMNMN(test));
+    }
+}
