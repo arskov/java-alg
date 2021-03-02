6e22712307b5feb73067f56c1957d958fefc8cdd
Arseny Kovalchuk <ars.kov@gmail.com>
2021-03-03 02:29:35 +0300

    Leetcode. 670. Maximum Swap

---
+++ lib/src/main/java/com/github/akovalchuk/MaximumSwap.java
@@ -0,0 +1,49 @@
+package com.github.akovalchuk;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 670. Maximum Swap
+ */
+public class MaximumSwap {
+
+    public int maximumSwap(int num) {
+        var nums = new ArrayList<Integer>();
+        int[] idxs = new int[10];
+        Arrays.fill(idxs, -1);
+        int tmp = num;
+        while (tmp > 0) {
+            int i = tmp % 10;
+            nums.add(i);
+            tmp /= 10;
+        }
+        Collections.reverse(nums);
+        for (int i = 0; i < nums.size(); i++)
+            idxs[nums.get(i)] = i;
+        outer: for (int i = 0; i < nums.size(); i++) {
+            for (int j = 9; j > nums.get(i); j--) {
+                if (idxs[j] > i) {
+                    Collections.swap(nums, i, idxs[j]);
+                    break outer;
+                }
+            }
+        }
+        int res = 0;
+        for (var n : nums) {
+            res *= 10;
+            res += n;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        var s = new MaximumSwap();
+        System.out.println(s.maximumSwap(21));
+        System.out.println(s.maximumSwap(12));
+        System.out.println(s.maximumSwap(212));
+        System.out.println(s.maximumSwap(2124));
+        System.out.println(s.maximumSwap(321));
+    }
+}
