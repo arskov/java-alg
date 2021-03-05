e8d5a11251c12fe434865cb9b55503575366726f
Arseny Kovalchuk <ars.kov@gmail.com>
2021-03-05 21:02:59 +0300

    Leetcode. 152. Maximum Product Subarray

---
+++ lib/src/main/java/com/github/akovalchuk/MaxProductSubArray.java
@@ -0,0 +1,30 @@
+package com.github.akovalchuk;
+
+/**
+ * 152. Maximum Product Subarray
+ */
+public class MaxProductSubArray {
+    
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int max = nums[0];
+        int min = nums[0];
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            int a = max * nums[i];
+            int b = min * nums[i];
+            max = Math.max(nums[i], Math.max(a, b));
+            min = Math.min(nums[i], Math.min(a, b));
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        var s = new MaxProductSubArray();
+        int[] test1 = {-2,3,-4};
+        int[] test2 = {1,2,-4,6};
+        System.out.println(s.maxProduct(test1));
+        System.out.println(s.maxProduct(test2));
+    }
+}
