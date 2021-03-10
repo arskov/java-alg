b415adc2b8cb61ccc093651a1fe1ac36c081f7b2
Arseny Kovalchuk <ars.kov@gmail.com>
2021-03-10 23:41:34 +0300

    Leetcode. 45. Jump Game II

---
+++ lib/src/main/java/com/github/akovalchuk/JumpGame2.java
@@ -0,0 +1,33 @@
+package com.github.akovalchuk;
+
+import java.util.Arrays;
+
+/**
+ * 45. Jump Game II
+ */
+public class JumpGame2 {
+
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < len - 1; i++) {
+            int last = Math.min(len - 1, i + nums[i]);
+            for (int j = i; j <= last; j++) {
+                dp[j] = Math.min(dp[j], dp[i] + 1);
+            }
+        }
+        return dp[len - 1];
+    }
+
+    public static void main(String[] args) {
+        var game = new JumpGame2();
+        int[] test1 = {2,3,1,1,4};
+        System.out.println(game.jump(test1));
+        int[] test2 = {2,3,0,1,4};
+        System.out.println(game.jump(test2));
+    }
+
+}
