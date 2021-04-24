276d869dfa36e9e361e8eaf3f91c2032516f2741
Arseny Kovalchuk <ars.kov@gmail.com>
2021-04-24 22:01:37 +0300

    Educative.io. Search in Rotated Array (medium)

--- lib/src/main/java/com/github/akovalchuk/SearchRotatedArray.java
+++ lib/src/main/java/com/github/akovalchuk/SearchRotatedArray.java
@@ -1,32 +1,39 @@
 package com.github.akovalchuk;
 
+/**
+ * Educative.io. Search in Rotated Array (medium)
+ */
 public class SearchRotatedArray {
 
-    static int binarySearchRotated(int[] arr, int key) {
+    public static int binarySearchRotated(int[] arr, int key) {
+        if (arr == null || arr.length == 0)
+            return -1;
+        int pivot = findPivot(arr);
+        if (key >= arr[0]) {
+            return binarySearch(arr, 0, pivot - 1, key);
+        } else {
+            return binarySearch(arr, pivot, arr.length - 1, key);
+        }
+    }
+
+    private static int findPivot(int[] arr) {
         int lo = 0;
         int hi = arr.length - 1;
-        int mi = 0;
         while (lo < hi) {
-            mi = lo + (hi - lo) / 2;
+            int mi = lo + (hi - lo) / 2;
             if (arr[mi] > arr[hi]) {
                 lo = mi + 1;
             } else {
-                hi = mi - 1;
+                hi = mi;
             }
         }
-        int startIdx = hi + 1;
-        if (key == arr[startIdx])
-            return startIdx;
-        if (key > arr[startIdx] && key <= arr[arr.length - 1]) {
-            lo = startIdx + 1;
-            hi = arr.length - 1;
-        } else {
-            lo = 0;
-            hi = startIdx - 1;
-        }
+        return hi;
+    }
+
+    private static int binarySearch(int[] arr, int lo, int hi, int key) {
         while (lo <= hi) {
-            mi = lo + (hi - lo) / 2;
-            if (key == arr[mi]) {
+            int mi = lo + (hi - lo) / 2;
+            if (arr[mi] == key) {
                 return mi;
             } else if (key < arr[mi]) {
                 hi = mi - 1;
@@ -38,6 +45,9 @@ public class SearchRotatedArray {
     }
 
     public static void main(String[] args) {
+        System.out.println(binarySearchRotated(new int[] { 10, 15, 1, 3, 8 }, 15));
+        System.out.println(binarySearchRotated(new int[] { 4, 5, 7, 9, 10, -1, 2 }, 10));
+        System.out.println("--");
         int[] test = { 162, 176, 188, 199, 200, 210, 222, 1, 10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155 };
         System.out.println(binarySearchRotated(test, 162));
         System.out.println(binarySearchRotated(test, 1));
