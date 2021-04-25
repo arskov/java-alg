5f91e65902cc2935c2c2d3248273ebfb73282076
Arseny Kovalchuk <ars.kov@gmail.com>
2021-04-25 12:09:30 +0300

    Educative.io. Connect Ropes (easy)

---
+++ lib/src/main/java/com/github/akovalchuk/ConnectRopes.java
@@ -0,0 +1,37 @@
+package com.github.akovalchuk;
+
+import java.util.PriorityQueue;
+
+/**
+ * Educative.io. Connect Ropes (easy)
+ * 
+ * 1167. Minimum Cost to Connect Sticks
+ */
+public class ConnectRopes {
+    public static int minimumCostToConnectRopes(int[] ropeLengths) {
+        if (ropeLengths == null || ropeLengths.length == 0)
+            return 0;
+        if (ropeLengths.length == 1)
+            return ropeLengths[0];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < ropeLengths.length; i++) {
+            pq.offer(ropeLengths[i]);
+        }
+        int totalSum = 0;
+        while (pq.size() > 1) {
+            int tmp = pq.poll() + pq.poll();
+            pq.offer(tmp);
+            totalSum += tmp;
+        }
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        int result = ConnectRopes.minimumCostToConnectRopes(new int[] { 1, 3, 11, 5 });
+        System.out.println("Minimum cost to connect ropes: " + result);
+        // result = ConnectRopes.minimumCostToConnectRopes(new int[] { 3, 4, 5, 6 });
+        // System.out.println("Minimum cost to connect ropes: " + result);
+        // result = ConnectRopes.minimumCostToConnectRopes(new int[] { 1, 3, 11, 5, 2 });
+        // System.out.println("Minimum cost to connect ropes: " + result);
+    }
+}
