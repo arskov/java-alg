66e2b554d69dc0b827ad5627d0e04df2773ddb64
Arseny Kovalchuk <ars.kov@gmail.com>
2021-04-26 02:01:03 +0300

    Educative.io. Connect Ropes (easy)

--- lib/src/main/java/com/github/akovalchuk/ConnectRopes.java
+++ lib/src/main/java/com/github/akovalchuk/ConnectRopes.java
@@ -29,9 +29,9 @@ public class ConnectRopes {
     public static void main(String[] args) {
         int result = ConnectRopes.minimumCostToConnectRopes(new int[] { 1, 3, 11, 5 });
         System.out.println("Minimum cost to connect ropes: " + result);
-        // result = ConnectRopes.minimumCostToConnectRopes(new int[] { 3, 4, 5, 6 });
-        // System.out.println("Minimum cost to connect ropes: " + result);
-        // result = ConnectRopes.minimumCostToConnectRopes(new int[] { 1, 3, 11, 5, 2 });
-        // System.out.println("Minimum cost to connect ropes: " + result);
+        result = ConnectRopes.minimumCostToConnectRopes(new int[] { 3, 4, 5, 6 });
+        System.out.println("Minimum cost to connect ropes: " + result);
+        result = ConnectRopes.minimumCostToConnectRopes(new int[] { 1, 3, 11, 5, 2 });
+        System.out.println("Minimum cost to connect ropes: " + result);
     }
 }
